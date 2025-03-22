2dff46e5e2e39e2ea2b757cdd613a9b05be9ce47
xygyl <xygyl@proton.me>
2025-03-21 20:23:49 -0500

    toml parsing setup

---
+++ src/config.rs
@@ -0,0 +1,54 @@
+use serde::Deserialize;
+// use std::fs;
+
+#[derive(Deserialize)]
+pub struct Colors {
+    pub border: String,
+    pub directory_path: String,
+    pub playback_speed: String,
+    pub currently_playing: String,
+    pub paused: String,
+    pub muted: String,
+    pub volume: String,
+    pub separators: String,
+    pub filesystem_directory: String,
+    pub filesystem_file: String,
+}
+
+#[derive(Deserialize)]
+pub struct ConfigData {
+    pub colors: Colors,
+}
+
+/* pub struct Config {
+    pub border_color: String,
+    pub directory_path_color: String,
+    pub playback_speed_color: String,
+    pub currently_playing_color: String,
+    pub paused_color: String,
+    pub muted_color: String,
+    pub volume_color: String,
+    pub separators_color: String,
+    pub filesystem_directory_color: String,
+    pub filesystem_file_color: String,
+}
+
+impl Config {
+    pub fn new(file_path: &str) -> Self {
+        let content = fs::read_to_string(file_path).expect("Failed to read config file path");
+        let data: ConfigData = toml::from_str(&content).expect("Failed to parse toml");
+
+        Config {
+            border_color: data.colors.border,
+            directory_path_color: data.colors.directory_path,
+            playback_speed_color: data.colors.playback_speed,
+            currently_playing_color: data.colors.currently_playing,
+            paused_color: data.colors.paused,
+            muted_color: data.colors.muted,
+            volume_color: data.colors.volume,
+            separators_color: data.colors.separators,
+            filesystem_directory_color: data.colors.filesystem_directory,
+            filesystem_file_color: data.colors.filesystem_file,
+        }
+    }
+} */
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+// use serde::Deserialize;
+use std::{env, fs};
+use toml;
+
+use crate::config::ConfigData;
+
 mod audio_playing;
 mod browser;
+mod config;
 mod sink_handling;
 mod tui;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let home_dir = env::var("HOME").expect("Couldn't find home directory");
+    let config_path = format!("{}/.config/rmpr/config.toml", home_dir);
+
+    let config = fs::read_to_string(&config_path)
+        .expect(&format!("Failed to read config file at {}", config_path));
+    let config_data: ConfigData = toml::from_str(&config).expect("Failed to parse TOML config");
+
+    println!("{}", config_data.colors.border);
+    println!("{}", config_data.colors.directory_path);
+    println!("{}", config_data.colors.playback_speed);
+    println!("{}", config_data.colors.currently_playing);
+    println!("{}", config_data.colors.paused);
+    println!("{}", config_data.colors.muted);
+    println!("{}", config_data.colors.volume);
+    println!("{}", config_data.colors.separators);
+    println!("{}", config_data.colors.filesystem_directory);
+    println!("{}", config_data.colors.filesystem_file);
+
     tui::run_tui()
 }
